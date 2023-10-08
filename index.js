18cacbceffa5012012d7e1671cd3b8bfd769c8b7
Theresa Tran <theresa1194@gmail.com>
2023-10-08 21:13:38 +1100

    done

---
+++ index.js
@@ -0,0 +1,30 @@
+const divide = function(a, b) {
+    
+    return a/b;
+}
+    let a = 2000;
+    let b = 100;
+
+    console.log(divide);
+
+
+
+const square = (x) => {
+        return x * x;
+}
+    let x = 2;
+
+    square(2);
+    console.log(square);
+
+
+const add = (c, d) => {
+    return c + d;
+}
+
+let c = 3;
+let d = 4;
+
+
+console.log(add);
+
